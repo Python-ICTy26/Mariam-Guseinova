2f1fc3f37a27ad89d57da39793e22201456d4472
Mariam-Guseinova <mariam.guseinova.itmo@gmail.com>
2022-10-04 17:14:30 +0300

    update

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,46 +1,17 @@
-import typing as tp
-
-
-def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
-    """
-    Encrypts plaintext using a Caesar cipher.
-
-    >>> encrypt_caesar("PYTHON")
-    'SBWKRQ'
-    >>> encrypt_caesar("python")
-    'sbwkrq'
-    >>> encrypt_caesar("Python3.6")
-    'Sbwkrq3.6'
-    >>> encrypt_caesar("")
-    ''
-    """
+def encrypt_caesar(plaintext, shift=3):
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for c in plaintext:
+        n = 0
+        if ord('A') <= ord(c) <= ord('Z'): ciphertext += chr(((ord(c) - ord('A') + shift) % 26) + ord('A'))
+        elif ord('a') <= ord(c) <= ord('z'): ciphertext += chr(((ord(c) - ord('a') + shift) % 26) + ord('a'))
+        else: ciphertext += c 
     return ciphertext
 
-
-def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
-    """
-    Decrypts a ciphertext using a Caesar cipher.
-
-    >>> decrypt_caesar("SBWKRQ")
-    'PYTHON'
-    >>> decrypt_caesar("sbwkrq")
-    'python'
-    >>> decrypt_caesar("Sbwkrq3.6")
-    'Python3.6'
-    >>> decrypt_caesar("")
-    ''
-    """
+def decrypt_caesar(ciphertext, shift=3):
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for c in ciphertext:
+        n = 0
+        if ord('A') <= ord(c) <= ord('Z'): plaintext += chr(((ord(c) - ord('A') - shift) % 26) + ord('A'))
+        elif ord('a') <= ord(c) <= ord('z'): plaintext += chr(((ord(c) - ord('a') - shift) % 26) + ord('a'))
+        else: plaintext += c 
     return plaintext
-
-
-def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
-    """
-    Brute force breaking a Caesar cipher.
-    """
-    best_shift = 0
-    # PUT YOUR CODE HERE
-    return best_shift
