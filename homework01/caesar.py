6f5a94d1454a22e56eb98c4dc120bcbdce4ca36b
Mariam-Guseinova <mariam.guseinova.itmo@gmail.com>
2022-10-04 17:23:43 +0300

    update1

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -2,16 +2,23 @@ def encrypt_caesar(plaintext, shift=3):
     ciphertext = ""
     for c in plaintext:
         n = 0
-        if ord('A') <= ord(c) <= ord('Z'): ciphertext += chr(((ord(c) - ord('A') + shift) % 26) + ord('A'))
-        elif ord('a') <= ord(c) <= ord('z'): ciphertext += chr(((ord(c) - ord('a') + shift) % 26) + ord('a'))
-        else: ciphertext += c 
+        if ord("A") <= ord(c) <= ord("Z"):
+            ciphertext += chr(((ord(c) - ord("A") + shift) % 26) + ord("A"))
+        elif ord("a") <= ord(c) <= ord("z"):
+            ciphertext += chr(((ord(c) - ord("a") + shift) % 26) + ord("a"))
+        else:
+            ciphertext += c
     return ciphertext
+
 
 def decrypt_caesar(ciphertext, shift=3):
     plaintext = ""
     for c in ciphertext:
         n = 0
-        if ord('A') <= ord(c) <= ord('Z'): plaintext += chr(((ord(c) - ord('A') - shift) % 26) + ord('A'))
-        elif ord('a') <= ord(c) <= ord('z'): plaintext += chr(((ord(c) - ord('a') - shift) % 26) + ord('a'))
-        else: plaintext += c 
+        if ord("A") <= ord(c) <= ord("Z"):
+            plaintext += chr(((ord(c) - ord("A") - shift) % 26) + ord("A"))
+        elif ord("a") <= ord(c) <= ord("z"):
+            plaintext += chr(((ord(c) - ord("a") - shift) % 26) + ord("a"))
+        else:
+            plaintext += c
     return plaintext
