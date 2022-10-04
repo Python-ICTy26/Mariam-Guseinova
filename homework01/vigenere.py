60b8dfa020c49c6e8f3fd49db7fdda6a5dfa8a36
Mariam-Guseinova <mariam.guseinova.itmo@gmail.com>
2022-10-04 21:48:21 +0300

    update

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -3,46 +3,47 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     for i in range(0, len(plaintext)):
         current_letter = ord(plaintext[i])
         if len(keyword) <= i:
-                sdvig = ord(keyword[i - len(keyword) * (i // len(keyword))])
-                if ord("A") <= sdvig <= ord("Z"):
-                        sdvig = sdvig - ord("A")
-                elif ord("a") <= sdvig <= ord("z"):
-                        sdvig = sdvig - ord("a")
+            sdvig = ord(keyword[i - len(keyword) * (i // len(keyword))])
+            if ord("A") <= sdvig <= ord("Z"):
+                sdvig = sdvig - ord("A")
+            elif ord("a") <= sdvig <= ord("z"):
+                sdvig = sdvig - ord("a")
         else:
-                sdvig = ord(keyword[i])
-                if ord("A") <= sdvig <= ord("Z"):
-                        sdvig = sdvig - ord("A")
-                elif ord("a") <= sdvig <= ord("z"):
-                        sdvig = sdvig - ord("a")
+            sdvig = ord(keyword[i])
+            if ord("A") <= sdvig <= ord("Z"):
+                sdvig = sdvig - ord("A")
+            elif ord("a") <= sdvig <= ord("z"):
+                sdvig = sdvig - ord("a")
         if ord("A") <= current_letter <= ord("Z"):
-                ciphertext += chr(((current_letter - ord("A") + sdvig) % 26) + ord("A"))
+            ciphertext += chr(((current_letter - ord("A") + sdvig) % 26) + ord("A"))
         elif ord("a") <= current_letter <= ord("z"):
-                ciphertext += chr(((current_letter - ord("a") + sdvig) % 26) + ord("a"))
+            ciphertext += chr(((current_letter - ord("a") + sdvig) % 26) + ord("a"))
         else:
-                ciphertext += plaintext[i]
+            ciphertext += plaintext[i]
 
     return ciphertext
-    
+
+
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     plaintext = ""
     for i in range(0, len(ciphertext)):
         current_letter = ord(ciphertext[i])
         if len(keyword) <= i:
-                sdvig = ord(keyword[i - len(keyword) * (i // len(keyword))])
-                if ord("A") <= sdvig <= ord("Z"):
-                        sdvig = sdvig - ord("A")
-                elif ord("a") <= sdvig <= ord("z"):
-                        sdvig = sdvig - ord("a")
+            sdvig = ord(keyword[i - len(keyword) * (i // len(keyword))])
+            if ord("A") <= sdvig <= ord("Z"):
+                sdvig = sdvig - ord("A")
+            elif ord("a") <= sdvig <= ord("z"):
+                sdvig = sdvig - ord("a")
         else:
-                sdvig = ord(keyword[i])
-                if ord("A") <= sdvig <= ord("Z"):
-                        sdvig = sdvig - ord("A")
-                elif ord("a") <= sdvig <= ord("z"):
-                        sdvig = sdvig - ord("a")
+            sdvig = ord(keyword[i])
+            if ord("A") <= sdvig <= ord("Z"):
+                sdvig = sdvig - ord("A")
+            elif ord("a") <= sdvig <= ord("z"):
+                sdvig = sdvig - ord("a")
         if ord("A") <= current_letter <= ord("Z"):
-                plaintext += chr(((current_letter - ord("A") - sdvig) % 26) + ord("A"))
+            plaintext += chr(((current_letter - ord("A") - sdvig) % 26) + ord("A"))
         elif ord("a") <= current_letter <= ord("z"):
-                plaintext += chr(((current_letter - ord("a") - sdvig) % 26) + ord("a"))
+            plaintext += chr(((current_letter - ord("a") - sdvig) % 26) + ord("a"))
         else:
-                plaintext += ciphertext[i]
+            plaintext += ciphertext[i]
     return plaintext
