3f4755f86fc070bc0c9ffb913cecf2702ad22327
Mariam-Guseinova <mariam.guseinova.itmo@gmail.com>
2023-01-17 16:40:15 +0300

    6

--- homework04/pyvcs/porcelain.py
+++ homework04/pyvcs/porcelain.py
@@ -14,29 +14,35 @@ def add(gitdir: pathlib.Path, paths: tp.List[pathlib.Path]) -> None:
 
 
 def commit(gitdir: pathlib.Path, message: str, author: tp.Optional[str] = None) -> str:
-    return commit_tree(gitdir, write_tree(gitdir, read_index(gitdir)), message, author)
+    index = read_index(gitdir)
+    return commit_tree(gitdir, write_tree(gitdir, index), message, author=author)
 
 
 def checkout(gitdir: pathlib.Path, obj_name: str) -> None:
-    file = gitdir / "refs" / "heads" / obj_name
-    if file.exists():
-        with open(file, "w") as f:
-            obj_name = f.read()
-    ind = read_index(gitdir)
-    for i in ind:
-        if pathlib.Path(i.name).is_file():
-            if "/" in i.name:
-                index = i.name.find("/")
-                shutil.rmtree(i.name[:index])
-            else:
-                os.remove(i.name)
-    path = gitdir / "objects" / obj_name[:2] / obj_name[2:]
-    with open(path, "rb") as f1:
-        sha = commit_parse(f1.read()).decode()
-    for f2 in find_tree_files(sha, gitdir):
-        if "/" in f2[0]:
-            index = f2[0].find("/")
-            dirname = f2[0][:index]
-            pathlib.Path(dirname).absolute().mkdir()
-        with open(f2[0], "w") as f3:
-            f3.write(read_object(f2[1], gitdir)[1].decode())
+    update_ref(gitdir, "HEAD", obj_name)
+
+    hash = commit_parse(read_object(obj_name, gitdir)[1])
+    files = find_tree_files(hash, gitdir)
+
+    names = []
+    for i in read_index(gitdir):
+        names.append(i.name)
+
+    update_index(gitdir, [pathlib.Path(i[1]) for i in files], write=True)
+
+    for j in names:
+        first = j.split("/")[0]
+        if pathlib.Path(first).is_dir():
+            shutil.rmtree(first)
+        else:
+            if pathlib.Path(first).exists():
+                os.remove(first)
+
+    for z in files:
+        if "/" in z[1]:
+            el = os.path.split(z[1])[0]
+            if not pathlib.Path(el).exists():
+                os.makedirs(el)
+        f = open(z[1], "wb")
+        f.write(read_object(z[0], gitdir)[1])
+
